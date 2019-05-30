19e56df5878a8b474de46ad9f7ee9c643e507f44
ilushin <andreialexandrovich@gmail.com>
2019-05-30 21:51:12 +0300

    Верстка + gmaps

---
+++ LandRegistry/LandRegistry/wwwroot/js/call-scripts/lands-page.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+
+    window.landsPage = new Vue({
+        el: ".lands-page",
+        data: function () {
+            return {
+
+                selectedQuestionId: null
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        async mounted() {
+
+        }
+    });
+
+
+})
+
+window.initMap = function () {
+    window.map = new google.maps.Map(document.getElementById('map-placeholder'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+};

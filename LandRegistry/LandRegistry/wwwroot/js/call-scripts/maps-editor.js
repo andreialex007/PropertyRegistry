f8acd821d4df25964ecbb983b444615fdeffc7ec
ilushin <andreialexandrovich@gmail.com>
2019-06-01 01:17:52 +0300

    Доделки по гуглкартам

--- LandRegistry/LandRegistry/wwwroot/js/call-scripts/maps-editor.js
+++ LandRegistry/LandRegistry/wwwroot/js/call-scripts/maps-editor.js
@@ -3,34 +3,97 @@
     window.mapApi = function () {
         let self = {};
 
+        self.currentShape = null;
+
+        self.disableDrawing = function () {
+            window.drawingManager.setMap(null);
+            if (!!self.currentShape) {
+                self.currentShape.setMap(null);
+                self.currentShape = null;
+            }
+        }
+
+        self.startDrawing = function () {
+            window.drawingManager.setMap(window.map);
+            drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYGON);
+        }
+
+        self.startDrawingFromCoordinates = function () {
+            self.setShapeFromCoordinates([
+                { lat: 25.774, lng: -80.190 },
+                { lat: 18.466, lng: -66.118 },
+                { lat: 32.321, lng: -64.757 }
+            ]);
+        }
+
+        self.getShapeCoordinates = function () {
+            return mapApi.currentShape.getPath().getArray().map(x => ({ lat: x.lat(), lng: x.lng() }));
+        }
+
+        self.setShapeFromCoordinates = function (triangleCoords) {
+            
+            self.currentShape = new google.maps.Polygon({
+                paths: triangleCoords,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.10,
+                editable: true,
+            });
+            self.currentShape.setMap(window.map);
+
+        }
+
         self.init = async function () {
+            setTimeout(function () {
+                var latlng = new google.maps.LatLng(39.305, -76.617);
+                window.map = new google.maps.Map(document.getElementById('map-placeholder'), {
+                    center: latlng,
+                    zoom: 12
+                });
 
-            await utils.wait(200);
-
-            var latlng = new google.maps.LatLng(39.305, -76.617);
-            window.map = new google.maps.Map(document.getElementById('map-placeholder'), {
-                center: latlng,
-                zoom: 12
-            });
-
-            window.drawingManager = new google.maps.drawing.DrawingManager({
-                drawingMode: google.maps.drawing.OverlayType.MARKER,
-                drawingControl: true,
-                drawingControlOptions: {
-                    position: google.maps.ControlPosition.TOP_CENTER,
-                    drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
-                },
-                markerOptions: { icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png' },
-                circleOptions: {
-                    fillColor: '#ffff00',
-                    fillOpacity: 1,
-                    strokeWeight: 5,
-                    clickable: false,
+                var polyOptions = {
+                    strokeWeight: 0,
+                    fillOpacity: 0.45,
                     editable: true,
-                    zIndex: 1
-                }
-            });
-            drawingManager.setMap(window.map);
+                    draggable: true
+                };
+
+
+                window.drawingManager = new google.maps.drawing.DrawingManager({
+                    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+                    markerOptions: {
+                        draggable: true
+                    },
+                    polylineOptions: {
+                        editable: true,
+                        draggable: true
+                    },
+                    rectangleOptions: polyOptions,
+                    circleOptions: polyOptions,
+                    polygonOptions: polyOptions,
+                    map: window.map,
+                    drawingControlOptions: {
+                        position: google.maps.ControlPosition.TOP_CENTER,
+                        drawingModes: []
+                    }
+
+                });
+
+                self.disableDrawing();
+
+
+
+                google.maps.event.addListener(drawingManager, 'overlaycomplete', function (e) {
+                    self.currentShape = e.overlay;
+                    self.currentShape.type = e.type;
+                    if (e.type !== google.maps.drawing.OverlayType.MARKER) {
+                        drawingManager.setDrawingMode(null);
+                    }
+                });
+            },
+                200);
         }
 
         self.init();
